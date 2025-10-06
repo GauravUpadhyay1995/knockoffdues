eb53dfb4baf18cbc2c6af0fb94be25ff600def7f
GAURAV UPADHYAY <142603807+GauravUpadhyay1995@users.noreply.github.com>
2025-10-06 13:16:03 +0530

    remove cronjob

--- src/lib/cronScheduler.ts
+++ src/lib/cronScheduler.ts
@@ -1,14 +1,14 @@
-import cron from "node-cron";
-import { GET as scheduleReminders } from "@/app/api/v1/admin/scheduler/reminders/route"; // adjust path
-import dayjs from "dayjs";
+// import cron from "node-cron";
+// import { GET as scheduleReminders } from "@/app/api/v1/admin/scheduler/reminders/route"; // adjust path
+// import dayjs from "dayjs";
 
-// Schedule task at 12:05 AM daily
-cron.schedule("45 14 * * *", async () => {
-    console.log(`[${dayjs().format()}] Running monthly reminder rollover cron...`);
-    try {
-        const response = await scheduleReminders();
-        console.log("Cron completed:", response);
-    } catch (err) {
-        console.error("Cron error:", err);
-    }
-});
+// // Schedule task at 12:05 AM daily
+// cron.schedule("45 14 * * *", async () => {
+//     console.log(`[${dayjs().format()}] Running monthly reminder rollover cron...`);
+//     try {
+//         const response = await scheduleReminders();
+//         console.log("Cron completed:", response);
+//     } catch (err) {
+//         console.error("Cron error:", err);
+//     }
+// });
