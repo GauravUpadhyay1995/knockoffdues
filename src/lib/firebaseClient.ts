d01f0ba79c66ec7813166740421d2be36520c946
GAURAV UPADHYAY <142603807+GauravUpadhyay1995@users.noreply.github.com>
2026-02-25 14:05:05 +0530

    just comment

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase, connectDatabaseEmulator, ref, onValue } from "firebase/database";
-
+// this is comment 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
